fbe7e6cd42520cde21300507152cd26202a4c95c
untra <untra.sam@gmail.com>
2020-01-07 23:25:11 -0700

    .js stands for jesus 0.1.19

--- src/naiveasync/index.tsx
+++ src/naiveasync/index.tsx
@@ -1,3 +1,3 @@
-export { naiveAsyncEmoji, NaiveAsyncSlice, NaiveAsyncState, NaiveAsyncFunction, naiveAsyncInitialState as initialAsyncableState } from './actions'
+export { naiveAsyncEmoji, NaiveAsyncSlice, NaiveAsyncState, NaiveAsyncFunction, naiveAsyncInitialState } from './actions'
 export { naiveAsyncLifecycle, naiveAsyncMiddleware, naiveAsyncReducer, AsyncLifecycle, naiveAsyncInitialSlice } from './controllable'
 export { NaiveAsync } from './naiveasync'
