47ea05a8ccb8fd211cdbe89d18a0f62d3632b7fb
untra <untra.sam@gmail.com>
2020-01-07 23:18:16 -0700

    rename a few functions, compile with commonjs

--- src/naiveasync/index.tsx
+++ src/naiveasync/index.tsx
@@ -1,3 +1,3 @@
 export { naiveAsyncEmoji, NaiveAsyncSlice, NaiveAsyncState, NaiveAsyncFunction, naiveAsyncInitialState as initialAsyncableState } from './actions'
-export { naiveAsyncLifecycle, naiveAsyncMiddleware as asyncableMiddleware, naiveAsyncReducer as asyncableReducer, AsyncLifecycle, naiveAsyncInitialSlice } from './controllable'
+export { naiveAsyncLifecycle, naiveAsyncMiddleware, naiveAsyncReducer, AsyncLifecycle, naiveAsyncInitialSlice } from './controllable'
 export { NaiveAsync } from './naiveasync'
