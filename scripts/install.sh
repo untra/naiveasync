a8199a91474fa828871e4c0a96230a12bee94a4d
untra <sam.volin@stackhawk.com>
2020-05-18 13:47:58 -0600

    remove that one test

---
+++ scripts/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+. ./scripts/common.sh
+
+mkdir dist
+npm install
---
+++ scripts/package.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+. ./scripts/common.sh
+
+ONLY_PUSH_ARTIFACT_PATH="pushes"
+
+echo "building naiveasync"
+PROTO_SPECS=$(ls ../proto/stackhawk/proto/*.proto)
+npm build
+if ! bd_isdev ; then
+  if [[ ! -z ${ARTIFACT_PATH+x} && ${ARTIFACT_PATH} == ${ONLY_PUSH_ARTIFACT_PATH} ]]; then
+    if [[ ${APP_ENV} = "prod" ]]; then
+      npm publish --access restricted
+    else
+      echo "skipping npm publish, will only run for production builds"
+    fi
+  else
+    echo "skipping npm publish, will only run on push builds, not PRs"
+  fi
+else
+  echo "publishing to npm is disabled from developer environments"
+fi
+
+cd ${CWD}
