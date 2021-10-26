bd89dbb718d18e8af9f2f8e9852283758ff853a2
untra <sam.volin@stackhawk.com>
2021-10-26 08:30:11 -0600

    Updates

---
+++ precache-manifest.379fa01ed5cecb2d01fce9265a7e1505.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f86aeded495dc9f16f5828883584ad4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a92ada99b748e1bf2fe",
+    "url": "/static/js/2.9695c2fa.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.9695c2fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adbe01bbfe977927f908",
+    "url": "/static/js/main.39687abc.chunk.js"
+  },
+  {
+    "revision": "6d7a51a134b735537e00",
+    "url": "/static/js/runtime-main.c5e07986.js"
+  }
+]);
